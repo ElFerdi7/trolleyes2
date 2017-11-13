bc0a7b874f3f95b2871bbe98af5477d4bd4cb7bd
Fernando <Fernando@192.168.1.70>
2017-11-13 19:42:16 +0100

    LineaPedido view

--- public_html/js/system/config/routes.js
+++ public_html/js/system/config/routes.js
@@ -60,5 +60,7 @@ trolleyes.config(['$routeProvider', function ($routeProvider) {
          $routeProvider.when('/producto/1/view/:id', {templateUrl: 'js/app/producto/1/view.html', controller: 'ProductoView1Controller', resolve: {auth: authenticationAdministratorPromise}});
         //--------------------------
         $routeProvider.when('/linea_pedido/1/plist/:page?/:rpp?', {templateUrl: 'js/app/linea_pedido/1/plist.html', controller: 'Linea_pedidoPList1Controller', resolve: {auth: authenticationAdministratorPromise}}); 
+        $routeProvider.when('/linea_pedido/1/view/:id', {templateUrl: 'js/app/linea_pedido/1/view.html', controller: 'Linea_pedidoView1Controller', resolve: {auth: authenticationAdministratorPromise}});
+
         $routeProvider.otherwise({redirectTo: '/'});
     }]);
